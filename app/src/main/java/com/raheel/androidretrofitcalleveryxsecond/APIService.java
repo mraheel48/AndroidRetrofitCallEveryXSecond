f737f7def535c4fff0ec3b8da85bd911eafd40ad
mraheeldev <m.raheeldev@gmail.com>
2022-03-08 10:36:50 +0500

    Initial commit

---
+++ app/src/main/java/com/raheel/androidretrofitcalleveryxsecond/APIService.java
@@ -0,0 +1,15 @@
+package com.raheel.androidretrofitcalleveryxsecond;
+
+
+import io.reactivex.Observable;
+import retrofit2.http.GET;
+import retrofit2.http.Path;
+
+public interface APIService {
+
+
+    String BASE_URL = "https://api.chucknorris.io/jokes/";
+
+    @GET("{path}")
+    Observable<Jokes> getRandomJoke(@Path("path") String path);
+}
